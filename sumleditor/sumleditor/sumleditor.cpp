86620fd59d3734ce5213511b7e165d2e01234a15
SashkaCosmonaut@gmail.com <SashkaCosmonaut@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-09-27 09:56:12 +0000

    Добавлен класс сцены для отрисовки диаграммы. Объект сцены помещен на главное окно, и протестирован его функционал (задан цвет фона, задействован инструмент "рука", помещены две линии).

--- sumleditor/sumleditor/sumleditor.cpp
+++ sumleditor/sumleditor/sumleditor.cpp
@@ -1,13 +1,17 @@
 #include "stdafx.h"
 #include "sumleditor.h"
 
+
 sumleditor::sumleditor(QWidget *parent, Qt::WFlags flags)
 	: QMainWindow(parent, flags)
 {
 	ui.setupUi(this);
+
+	diagram = new GraphWidget(this);		// Создание сцены 
+	ui.gridLayout->addWidget(diagram);		// Помещение его на grid layout в главном окне 
 }
 
 sumleditor::~sumleditor()
 {
-
+	delete diagram;
 }
