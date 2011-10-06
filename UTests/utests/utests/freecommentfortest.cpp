f03d43777d217e17df8178f35e63b2f6f865a067
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-10-06 06:42:36 +0000

    Протестирован метод класса свободных комментариев используемый для сохранения свободных комментариев в xml файл.

---
+++ UTests/utests/utests/freecommentfortest.cpp
@@ -0,0 +1,51 @@
+#include "freecommentfortest.h"
+
+FreeCommentForTest::FreeCommentForTest()
+{
+	text = "text";
+	name = "name";
+	x = -1;
+	y = -1;
+	z = -1;
+}
+
+FreeCommentForTest::FreeCommentForTest(QString _text, QString _name, int _x, int _y, int _z)
+{
+	text = _text;
+	name = _name;
+	x = _x;
+	y = _y;
+	z = _z;
+}
+
+FreeCommentForTest::~FreeCommentForTest()
+{
+
+}
+
+QDomElement FreeCommentForTest::save(QDomDocument &domDoc) const
+{
+	QDomElement element = domDoc.createElement("freecomment");
+
+	QDomAttr attr = domDoc.createAttribute("text");
+	attr.setValue(text.toUtf8());
+	element.setAttributeNode(attr);
+
+	attr = domDoc.createAttribute("name");
+	attr.setValue(name.toUtf8());
+	element.setAttributeNode(attr);
+
+	attr = domDoc.createAttribute("x");
+	attr.setValue(QString::number(x));
+	element.setAttributeNode(attr);
+
+	attr = domDoc.createAttribute("y");
+	attr.setValue(QString::number(y));
+	element.setAttributeNode(attr);
+
+	attr = domDoc.createAttribute("z");
+	attr.setValue(QString::number(z));
+	element.setAttributeNode(attr);
+
+	return element;
+}
--- UTests/utests/utests/main.cpp
+++ UTests/utests/utests/main.cpp
@@ -1,13 +1,98 @@
 #include <QtTest>
 #include "freecommentfortest.h"
 
+Q_DECLARE_METATYPE(FreeCommentForTest)
+
 class Test_SUMLEditorProject : public QObject
 {
 	Q_OBJECT
 
+private:
+
+	/* Функция считывания xml файла в строку. */
+	QString readXmlFile (QString filename)
+	{
+		QString result = "";
+		QFile file(filename);
+
+		if (file.open(QIODevice::ReadOnly))
+		{
+			QTextStream stream(&file);
+			result = stream.readAll();
+			file.close();
+		}
+
+		return result;
+	}
+
+	/* Сохранения свободного комментария в xml файла. */
+	void save (QString filename, FreeCommentForTest fcft)
+	{
+		QDomDocument doc("test_comment");
+		QDomElement domElement = doc.createElement("comments");
+		doc.appendChild(domElement);
+
+		QDomElement dom = fcft.save(doc);
+		domElement.appendChild(dom);
+
+		QFile file(filename);
+		if (file.open(QIODevice::WriteOnly))
+		{
+			QTextStream(&file) << doc.toString();
+			file.close();
+		}
+	}
+
+	/* Функция проверки существования файла. */
+	bool isFileExist (QString filename)
+	{
+		bool flag = true;
+		QFile file(filename);
+
+		if (!file.exists())
+			flag = false;
+
+		return flag;
+	}
+
 private slots:
 
+	/* Тестирование сохранения в файл. */
+	void createXml_data();
+	void createXml();
+
 };
+
+void Test_SUMLEditorProject::createXml_data()
+{
+	QTest::addColumn<FreeCommentForTest>("comment");
+	QTest::addColumn<QString>("xmlStr");
+	QTest::addColumn<QString>("filename");
+
+	FreeCommentForTest f1 = FreeCommentForTest();
+	QString s2 = "firstTest.xml";
+	QString s1 = QString("<!DOCTYPE test_comment>\n<comments>\n <freecomment x=\"-1\" y=\"-1\" z=\"-1\" name=\"name\" text=\"text\"/>\n</comments>\n");
+
+	QTest::newRow("Test_1") << f1 << s1 << s2;
+
+	FreeCommentForTest f12 = FreeCommentForTest("My text", "My name", 10, 11, 12);
+	QString s22 = "firstTest.xml";
+	QString s12 = QString("<!DOCTYPE test_comment>\n<comments>\n <freecomment x=\"10\" y=\"11\" z=\"12\" name=\"My name\" text=\"My text\"/>\n</comments>\n");
+
+	QTest::newRow("Test_2") << f12 << s12 << s22;
+}
+void Test_SUMLEditorProject::createXml()
+{
+	QFETCH(FreeCommentForTest,comment);
+	QFETCH(QString,xmlStr);
+	QFETCH(QString,filename);
+
+	save(filename,comment);
+	QString result = readXmlFile(filename);
+
+	QCOMPARE(xmlStr, result);
+	QCOMPARE(isFileExist(filename),true);
+}
 
 QTEST_MAIN(Test_SUMLEditorProject)
 #include "main.moc"
