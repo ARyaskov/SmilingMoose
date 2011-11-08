97103ece2a6fc80b284bd90a155e57e08f732644
SashkaCosmonaut@gmail.com <SashkaCosmonaut@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-11-08 19:06:34 +0000

    Теперь окно программы развернуто на весь экран.

--- trunk/sumleditor/sumleditor/main.cpp
+++ trunk/sumleditor/sumleditor/main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
 	{
 		QApplication a(argc, argv);
 		Sumleditor w;
+		w.setWindowState(w.windowState() ^ Qt::WindowMaximized);	// Развернуть окно на весь экран
 		w.show();
 		return a.exec();
 	}
