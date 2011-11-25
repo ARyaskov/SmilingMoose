81d4e48df4e9578451c9e920b4701cd274e1b2fd
freakyblast@gmail.com <freakyblast@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-11-25 19:40:21 +0000

    Добавлен Java-проект.

---
+++ trunk/BBJ/src/bbj/BBJ.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbj;
+
+/**
+ *
+ * @author Lemon
+ */
+public class BBJ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
