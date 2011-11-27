cff8f1d83305035c2fdc224ce8261e5e57f70123
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-11-27 17:46:58 +0000

    Добавлен класс линии жизни.

---
+++ BBJ/src/bbj/virtualobjects/LifeLine.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbj.virtualobjects;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * Класс линии жизни.
+ * Наследуется от UMLObjects.
+ * @see {@link uml.virtualobjects.UMLObjects}
+ * @version 1.0
+ */
+public class LifeLine extends UMLObject {
+
+    @Override
+    public Element write(Document document) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void read(Element element) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
