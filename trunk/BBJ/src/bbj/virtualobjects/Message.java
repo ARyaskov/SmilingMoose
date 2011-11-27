a77c91ba6aa7aded9a50c7a87e26186180414a47
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-11-27 18:05:46 +0000

    Добавлен абстрактный класс сообщений.

---
+++ trunk/BBJ/src/bbj/virtualobjects/Message.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbj.virtualobjects;
+
+/**
+ * Абстрактный класс сообщений.
+ * Является суперклассом для сообщений.
+ * @version 1.0
+ */
+abstract public class Message extends UMLObject {
+    
+}
