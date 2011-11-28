a05972b8ab9d63254e3847053bf7fc70dc6eed19
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-11-28 05:34:12 +0000

    Добавлен графический объект - сцена.

---
+++ BBJ/src/bbj/graphicsobjects/Scene.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbj.graphicsobjects;
+
+import javax.swing.JPanel;
+
+/**
+ * Класс - сцена для отрисовки объектов диаграммы.
+ * @version 1.0
+ */
+public class Scene extends JPanel {
+    
+}
