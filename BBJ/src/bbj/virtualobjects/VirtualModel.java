81a39407627452e9d6a6082e2a182a7c5d945208
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-11-28 05:33:10 +0000

    Добавлен класс виртуального отображения графической диаграммы.

---
+++ BBJ/src/bbj/virtualobjects/VirtualModel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbj.virtualobjects;
+
+/**
+ * Класс виртуальной модели.
+ * Является виртуальным отображеием графической модели.
+ * @version 1.0
+ */
+public class VirtualModel {
+    
+}
