3f007ee5af268827ee32164c8b36d56bc55a11d8
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-11-28 05:39:40 +0000

    В класс виртуальной модели добавлены поля этого класса.

--- BBJ/src/bbj/virtualobjects/VirtualModel.java
+++ BBJ/src/bbj/virtualobjects/VirtualModel.java
@@ -4,11 +4,18 @@
  */
 package bbj.virtualobjects;
 
+import bbj.graphicsobjects.Scene;
+import java.util.ArrayList;
+
 /**
  * Класс виртуальной модели.
  * Является виртуальным отображеием графической модели.
  * @version 1.0
  */
 public class VirtualModel {
+    
+    /* Поля класса. */
+    private Scene m_scene; /** Указатель на графическое представление диаграммы. */
+    private ArrayList<UMLObject> m_objects; /** Контейнер объектов диаграммы. */
     
 }
