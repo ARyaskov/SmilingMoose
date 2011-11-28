d9bc397af5e2b7e529bbfe67ea2174560a5a9d1c
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-11-28 04:48:36 +0000

    Для поля "Отправитель" класса сообщения, добавлены функции доступа (get/set).

--- trunk/BBJ/src/bbj/virtualobjects/Message.java
+++ trunk/BBJ/src/bbj/virtualobjects/Message.java
@@ -14,4 +14,20 @@ abstract public class Message extends UMLObject {
     /* Поля класса. */
     private LifeLine m_sender; /** Линия жизин - отправитель сообщения. */
     private LifeLine m_receiver; /** Линия жизни - приемник сообщения. */
+    
+    /**
+     * Метод получения отправителя сообщения.
+     * @return Линия жизни, отправитель сообщения.
+     */
+    public LifeLine getSender () {
+        return m_sender;
+    }
+    
+    /**
+     * Метод задания отправителя сообщения.
+     * @param other Линия жизни, новый отправитель сообщения.
+     */
+    public void setSender (LifeLine other) {
+        m_sender = other;
+    }
 }
