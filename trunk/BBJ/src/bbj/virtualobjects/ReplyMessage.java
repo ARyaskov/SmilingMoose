f8a48c020f856bddac5b44a3195135fe760fece8
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-11-29 18:40:05 +0000

    Добавлен класс возвратных сообщений. Наследуется от абстрактного класса сообщений.

---
+++ trunk/BBJ/src/bbj/virtualobjects/ReplyMessage.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbj.virtualobjects;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * Класс сообщения уничтожения.
+ * Наследуется от Message.
+ * @see {@link uml.virtualobjects.Message}
+ * @version 1.0
+ */
+public class ReplyMessage extends Message {
+
+    @Override
+    public Element write(Document document) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void read(Element element) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
