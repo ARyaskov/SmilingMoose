afce9f2a8b01706ec9329b0556c36747bfac34a5
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-11-29 17:13:18 +0000

    Добавлен класс простых сообщений. Наследуется от абстрактного класса сообщений.

---
+++ trunk/BBJ/src/bbj/virtualobjects/SimpleMessage.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbj.virtualobjects;
+
+/**
+ * Класс простого сообщения.
+ * Наследуется от Message.
+ * @see {@link uml.virtualobjects.Message}
+ * @version 1.0
+ */
+public class SimpleMessage extends Message {
+    
+}
