98d1ac542ca0f590c59a4168ee506e37c4f0f3e0
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-11-30 17:04:27 +0000

    Добавлен класс точки в трехмерном пространстве.

---
+++ BBJ/src/bbj/graphicsobjects/Point3D.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbj.graphicsobjects;
+
+import java.awt.Point;
+
+/**
+ * Класс точки в трехмерных координатах.
+ * Наследуется от стандартного класса AWT: {@link java.awt.Point}.
+ * @version 1.0
+ */
+public class Point3D extends Point {
+    
+}
