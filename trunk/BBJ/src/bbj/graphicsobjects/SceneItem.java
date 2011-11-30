8a8dbbb110ea174b6a6e878d4842cbb62abea935
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-11-30 17:48:45 +0000

    Добавлен абстрактный графический класс UML объектов.

---
+++ trunk/BBJ/src/bbj/graphicsobjects/SceneItem.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbj.graphicsobjects;
+
+/**
+ * Класс графического отображения UML объектов.
+ * Наследуется от AWT {@link java.awt.Graphics2D}.
+ * @version 1.0
+ */
+abstract public class SceneItem {
+    
+}
