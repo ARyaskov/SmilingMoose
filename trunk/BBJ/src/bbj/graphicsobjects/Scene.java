6311dc5fd2ddd9784280c424186b6f707283d6ec
freakyblast@gmail.com <freakyblast@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-12-02 23:00:47 +0000

    Создан новый интерфейс. Без использования автоматического редактора компонентов.

--- trunk/BBJ/src/bbj/graphicsobjects/Scene.java
+++ trunk/BBJ/src/bbj/graphicsobjects/Scene.java
@@ -4,12 +4,53 @@
  */
 package bbj.graphicsobjects;
 
-import javax.swing.JPanel;
+import bbj.virtualobjects.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 
 /**
  * Класс - сцена для отрисовки объектов диаграммы.
  * @version 1.0
  */
 public class Scene extends JPanel {
+    private boolean isGrid;
+    private int gridFactor = 20;
+    private Color gridColor = new Color(128,128,128,64);
     
+    private VirtualModel m_model;
+    private ArrayList<Component> m_objects;
+    
+    public Scene(){
+        super();
+        isGrid = true;
+        m_model = new VirtualModel();
+        m_objects = new ArrayList<Component>();
+        
+        
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+            
+            if (isGrid)
+                makeGrid(g);
+            //isGrid = false;
+            //UILifeLine ll = new UILifeLine();
+            //ll.paintComponent(g);
+        }
+    
+    protected void makeGrid(Graphics g){
+        g.setColor(gridColor);
+        int _height=this.getHeight();
+        int _width=this.getWidth();
+        for (int i=0;i<200;i++){
+            g.drawLine(0, gridFactor*i, _width, gridFactor*i); // Горизонтальные
+        }
+        for (int i=0;i<200;i++){
+            g.drawLine(gridFactor*i, 0, gridFactor*i, _height); // Вертикальные
+        }
+        
+    }
 }
---
+++ trunk/BBJ/src/bbj/graphicsobjects/UILifeLine.java
@@ -0,0 +1,27 @@
+
+package bbj.graphicsobjects;
+
+import bbj.graphicsobjects.*;
+import bbj.virtualobjects.*;
+
+import java.awt.*;
+import javax.swing.*;
+/**
+ *
+ * @author Lemon
+ */
+public class UILifeLine extends JComponent{
+    
+    public UILifeLine(){
+        
+    }
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        GradientPaint gp = new GradientPaint(0, 0, Color.YELLOW,
+                                       0, 10, Color.WHITE);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setPaint(gp);
+        g2.fillRect(0, 0, 80, 30);       
+    }
+}
