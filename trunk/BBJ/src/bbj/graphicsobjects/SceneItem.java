5af82d50efd3dc954e83582d42fa3bb1e1a6cb0f
SashkaCosmonaut@gmail.com <SashkaCosmonaut@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-12-04 16:41:27 +0000

    Ну, понеслася! Реализованы методы для доступа к полям класса SceneItem, а также метод draw (он переименован в диаграмме) и еще разрешен старый конфликт с презентацией (может он у меня только был).

--- trunk/BBJ/src/bbj/graphicsobjects/SceneItem.java
+++ trunk/BBJ/src/bbj/graphicsobjects/SceneItem.java
@@ -4,6 +4,8 @@
  */
 package bbj.graphicsobjects;
 
+import java.awt.Graphics;
+
 /**
  * Класс графического отображения UML объектов.
  * Наследуется от AWT {@link java.awt.Graphics2D}.
@@ -12,6 +14,36 @@ package bbj.graphicsobjects;
 abstract public class SceneItem {
     
     /* Поля класса. */
-    private Point3D m_coordinate; /** Координата объекта. */
+    private Point3D m_coordinates; /** Координата объекта. */
     private boolean m_isSelected; /** Флаг выбора объекта фокусом. */
+    
+    /**
+     * Вернуть текущие координаты.
+     * @return Координаты объекта
+     */
+    public Point3D getCoordinates(){
+        return m_coordinates;
+    }
+    
+    /**
+     * Установить новые координаты
+     * @param newCoordinates Объект новых координат
+     */
+    public void setCoordinates(Point3D newCoordinates){
+        m_coordinates = newCoordinates;
+    }
+    
+    /**
+     * Выбрать объект
+     * @param flag Выбран ли объект
+     */
+    public void select(boolean flag){
+        m_isSelected = flag;
+    }
+    
+    /**
+     * Абстрактный метод отрисовки графического объекта
+     * @param g Объект, на котором будем рисовать
+     */
+    abstract public void draw(Graphics g);
 }
