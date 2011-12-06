9399ee45b955d6d717c7663294768a9574e8eace
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-12-06 16:15:33 +0000

    Реализован метод сохранения диаграммы в xml файл.

--- BBJ/src/bbj/BBJ.java
+++ BBJ/src/bbj/BBJ.java
@@ -309,6 +309,19 @@ public final class BBJ {
                 int returnVal = chooser.showSaveDialog(mainFrame);
                 if (returnVal == JFileChooser.APPROVE_OPTION) {
                     File file = chooser.getSelectedFile();
+                    try {
+                        canvas.getModel().save(file.getPath());
+                    } catch (ParserConfigurationException ex) {
+                        Logger.getLogger(BBJ.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (SAXException ex) {
+                        Logger.getLogger(BBJ.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(BBJ.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (TransformerConfigurationException ex) {
+                        Logger.getLogger(BBJ.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (TransformerException ex) {
+                        Logger.getLogger(BBJ.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
             }
         });
