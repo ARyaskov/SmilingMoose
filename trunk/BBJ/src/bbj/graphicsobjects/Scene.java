668bbfe2600cce23e7315a126bd9060f12e3017e
SashkaCosmonaut@gmail.com <SashkaCosmonaut@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-12-07 21:30:34 +0000

    Линии жизни с исполнителями были перерисованы.

--- trunk/BBJ/src/bbj/graphicsobjects/Scene.java
+++ trunk/BBJ/src/bbj/graphicsobjects/Scene.java
@@ -110,7 +110,7 @@ public final class Scene extends JPanel {
 
         UIRectLifeLine ll = new UIRectLifeLine(111, 111, "Линия жизни 1");
         
-        UIActorLifeLine al = new UIActorLifeLine(111, 333, "Актёр 1");
+        UIActorLifeLine al = new UIActorLifeLine(111, 333, "Исполнитель 1 тест");
                 
         ll.setEnded(true);
         this.add(testCommentChecked1);
