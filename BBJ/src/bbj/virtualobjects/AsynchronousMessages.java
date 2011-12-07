be503b5470759229a9c26639cf51edd162c453c5
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-12-07 18:09:06 +0000

    Добавлен класс асинхронных сообщений.

---
+++ BBJ/src/bbj/virtualobjects/AsynchronousMessages.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbj.virtualobjects;
+
+/**
+ * Класс асинхронного сообщения.
+ * Наследуется от Message.
+ * @see {@link uml.virtualobjects.Message}
+ * @version 1.0
+ */
+public class AsynchronousMessages extends Message {
+    
+}
