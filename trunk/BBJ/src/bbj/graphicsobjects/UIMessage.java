c0e7f33ce8f91a97fcf6bcb6d863fed50cea692c
SashkaCosmonaut@gmail.com <SashkaCosmonaut@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-12-08 19:26:25 +0000

    Добавлен абстрактный класс сообщения.

---
+++ trunk/BBJ/src/bbj/graphicsobjects/UIMessage.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbj.graphicsobjects;
+
+/**
+ * Абстрактный класс сообщения от одной линии жизни к другой.
+ * @author Alexander
+ */
+abstract public class UIMessage extends SceneItem {
+    
+}
