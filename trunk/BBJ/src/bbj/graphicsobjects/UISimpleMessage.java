53dab00a6042618c5b2fb265154a89e41bdbbacc
SashkaCosmonaut@gmail.com <SashkaCosmonaut@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-12-08 22:49:47 +0000

    Реализована отрисовка стрелки простого сообщения.

--- trunk/BBJ/src/bbj/graphicsobjects/UISimpleMessage.java
+++ trunk/BBJ/src/bbj/graphicsobjects/UISimpleMessage.java
@@ -4,6 +4,11 @@
  */
 package bbj.graphicsobjects;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 /**
  *
  * @author Alexander
@@ -15,8 +20,44 @@ public class UISimpleMessage extends UIMessage {
      * @param sender Отправитель
      * @param receiver Получатель
      */
-    public UISimpleMessage(UILifeLine sender, UILifeLine receiver ){
+    public UISimpleMessage(UILifeLine sender, UILifeLine receiver, int y ){
         m_sender = sender;
         m_receiver = receiver;
-    } 
+        this.y = y;
+    }
+
+    /**
+     * Отрисовка объекта
+     * @param g На чем рисовать
+     */
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2= (Graphics2D)g;
+        
+        int endX = m_receiver.x+60;         // Конечная точка
+        this.x = m_sender.x+60;             // Начальная точка
+        this.h = 25;                        // Высота объекта
+        this.w = endX - m_sender.x;         // Длина объекта
+
+        setBounds(x, y, w, h);              // Задаем границы
+        
+        g.setColor(Color.black);            // Исходный цвет
+        
+        g2.setStroke(new BasicStroke(2));   // Берем линию потолще
+        
+        g.drawLine(x, y, x+w-60, y);        // Рисуем линию сообщения
+        
+        // Рисуем стрелку в зависимости от расположения линии жизни
+        if (x > endX){
+            endX = m_sender.x+60;
+            g.drawLine(endX, y, endX-7, y+5);
+            g.drawLine(endX, y, endX-7, y-5);
+        }
+        else{
+            g.drawLine(x, y, x+7, y+5);
+            g.drawLine(x, y, x+7, y-5);
+        }
+    }
+    
+    
 }
