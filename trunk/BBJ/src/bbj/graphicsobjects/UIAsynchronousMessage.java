db71c23a8c5de6b5655beae6dd480eff1481f070
SashkaCosmonaut@gmail.com <SashkaCosmonaut@gmail.com@6dd2ee71-0441-70ab-6e60-75ddb2496318>
2011-12-10 09:54:30 +0000

    Добавлен класс асинхронных сообщений.

---
+++ trunk/BBJ/src/bbj/graphicsobjects/UIAsynchronousMessage.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbj.graphicsobjects;
+
+/**
+ *
+ * @author Alexander
+ */
+public class UIAsynchronousMessage extends UIMessage {
+
+    @Override
+    protected void setDefaultName() {
+
+    }
+    
+}
